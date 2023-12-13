70bd31ed9bcb9aa71fe1cfc13833f7a9463d54d3
Rishav Das <rishavdas27602@gmail.com>
2023-12-13 22:36:13 +0530

    today's commit

---
+++ Top_Interview_150/Easy/Remove_Element.java
@@ -0,0 +1,17 @@
+public class Remove_Element {
+
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        if (nums.length == 0) {
+            return 0;
+        } else {
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] != val) {
+                    nums[count++] = nums[i];
+                }
+            }
+        }
+        return count;
+    }
+
+}
