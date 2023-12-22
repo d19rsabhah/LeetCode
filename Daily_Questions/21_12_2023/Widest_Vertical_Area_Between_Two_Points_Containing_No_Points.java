81f79990601cabb5e6192212cbad0a8b6d2b2d89
Rishav Das <rishavdas27602@gmail.com>
2023-12-22 15:58:47 +0530

    new

---
+++ Daily_Questions/21_12_2023/Widest_Vertical_Area_Between_Two_Points_Containing_No_Points.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Widest_Vertical_Area_Between_Two_Points_Containing_No_Points {
+    public int maxWidthOfVerticalArea(int[][] points) {
+        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
+
+        int max_width = 0;
+
+        for (int i = 1; i < points.length; i++) {
+            int width = points[i][0] - points[i - 1][0];
+            max_width = Math.max(max_width, width);
+        }
+
+        return max_width;
+    }
+}
