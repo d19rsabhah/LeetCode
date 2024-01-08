dc6b83d3b80b1c73bd3eb7d3fc3c55268a066293
Rishav Das <rishavdas27602@gmail.com>
2024-01-08 23:29:41 +0530

    938. Range Sum of BST

---
+++ Daily_Questions/Year_2024/JANUARY/08_01_2024/Range_Sum_Of_BST.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Range_Sum_Of_BST {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (low <= root.val && root.val <= high) {
+            return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+        } else if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else {
+            return rangeSumBST(root.left, low, high);
+        }
+    }
+}
