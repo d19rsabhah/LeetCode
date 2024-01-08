64803486d34d4d9015f08ffcd22f7b78c69574dd
Rishav Das <rishavdas27602@gmail.com>
2024-01-08 20:01:24 +0530

    Delete Python directory

--- Python/Capital.py
+++
@@ -1,9 +0,0 @@
-str = "python is a good language"
-
-words = str.split()
-
-Capitalize = [word[:-1] + word[-1].upper() for word in words]
-
-ans = ' '.join(Capitalize)
-
-print(ans)   # Output : pythoN iS A gooD languagE
